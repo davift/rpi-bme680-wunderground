656bf8aefcba4d65621596b37cbd2a3b8d0a4a72
davift <73488542+davift@users.noreply.github.com>
2020-10-26 13:05:42 -0400

    Update read-all.py

--- read-all.py
+++ read-all.py
@@ -48,19 +48,18 @@ sensor.select_gas_heater_profile(0)
 # sensor.set_gas_heater_profile(200, 150, nb_profile=1)
 # sensor.select_gas_heater_profile(1)
 
+# ADD HERE THE CREDENTIALS TO THE DEVICE ALREADY ADDED TO THE WEATHER UNDERGROUND DASHBOARD (https://www.wunderground.com/member/devices):
+wu_id = 'ID'
+wu_key = 'Key'
+
 #print('\n\nPolling:')
 tem = 0
-tem2 = 0
 pre = 0
-pre2 = 0
 hum = 0
-hum2 = 0
 air = 0
-air2 = 0
 dew = 0
 try:
     count = 0
-    count2 = 1
     while True:
         if sensor.get_sensor_data():
             output = '{3:.0f}, {0:.2f} C, {1:.2f} hPa, {2:.2f} %'.format(sensor.data.temperature,sensor.data.pressure,sensor.data.humidity,count)
@@ -78,28 +77,16 @@ try:
 	count = count + 1
 	if count > 60:
 		dew = (((math.log(hum/60/100))+((17.62*tem/60)/(243.12+tem/60)))*243.12)/(17.62-((math.log(hum/60/100))+((17.62*tem/60)/(243.12+tem/60))))
-		# ITANAGRA
-		os.system('curl "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=IITANA3&PASSWORD=HCHTQT46&dateutc=now&action=updateraw&humidity={2:.1f}&tempf={0:.1f}&baromin={1:.1f}&aqi={3:.0f}&dewptf={4:.1f}"'.format(tem/60*9/5+32,pre/60/33.86,hum/60,air/60/1000,dew*9/5+32))
-                # TORONTO
-		#os.system('curl "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=ITORON119&PASSWORD=BzmIDdxw&dateutc=now&action=updateraw&humidity={2:.1f}&tempf={0:.1f}&baromin={1:.1f}&aqi={3:.0f}&dewptf={4:.1f}"'.format(tem/60*9/5+32,pre/60/33.86,hum/60,air/60/1000,dew*9/5+32))
+		os.system('curl "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID={5}&PASSWORD={6}&dateutc=now&action=updateraw&humidity={2:.1f}&tempf={0:.1f}&baromin={1:.1f}&aqi={3:.0f}&dewptf={4:.1f}"'.format(tem/60*9/5+32,pre/60/33.86,hum/60,air/60/1000,dew*9/5+32,wu_id,wu_key))
 		count = 1
-		count2 = count2 + 1
-                tem2 = tem2 + tem/60
-                pre2 = pre2 + pre/60
-                hum2 = hum2 + hum/60
-                air2 = air2 + air/60
                 tem = 0
                 pre = 0
                 hum = 0
                 air = 0
-	if count2 > 10:
-		os.system('curl "https://admin.dftorres.ca/weather.php?hum={2:.1f}&tem={0:.1f}&pre={1:.1f}&iaq={3:.0f}"'.format(tem2/10,pre2/10,hum2/10,air2/10000))
-		count2 = 1
-                tem = 0
-                pre = 0
-                hum = 0
-                air = 0
+		# COMMENT THIS LINE TO KEEP IT IN LOOPING OR USER CRON TO RELOAD IT EVERY MINUTE
 		exit()
+		# TO CREATE THE CRON RE-LOAD ROUTINE ISSUE THE COMMAND: crontab -e
+		# AND ADD THIS LINE AT THE END: * * * * * /home/pi/read-all.py > /dev/null 2>&1
 
         time.sleep(1)
 
